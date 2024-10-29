24e20795ed87321205518367a906a2fd03332d2e
shmuel keller <shmuli@shmuels-Air.attlocal.net>
2024-10-29 19:58:42 -0400

    in UseList i use .bind on all the functions that would previously get created every time UseList ran to make this function more effciant

--- src/hooks/List.tsx
+++ src/hooks/List.tsx
@@ -4,87 +4,92 @@ const rerenders = new Map();
 
 let current_key = 0;
 
+
+
+function triggerRerender(data_based_on: Array<any>) {
+  for (const rerender of rerenders.get(data_based_on).values()) {
+    console.log({ rerender });
+    rerender();
+  }
+}
+
+function deleteUser(id: number | any) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i].id === id) {
+      this.splice(i, 1);
+      break;
+    }
+  }
+  triggerRerender(this);
+}
+function addNewItem() {
+  const newUser = {
+    id: this.length + 1,
+    name: "Abhi",
+    age: 21,
+    country: "India",
+    city: "Delhi",
+  };
+  this.push(newUser);
+  triggerRerender(this);
+}
+function sortList(field: string) {
+  this.sort((a, b) => (a[field] > b[field] ? 1 : -1));
+  triggerRerender(this);
+}
+function clearList() {
+  this.splice(0, this.length);
+  triggerRerender(this);
+}
+function updateItem(id: number | any, key: string, value: any | Function) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i].id === id) {
+      if (typeof value === "function") {
+        this[i][key] = value(this[i][key]);
+      } else {
+        this[i][key] = value;
+      }
+      break;
+    }
+  }
+  triggerRerender(this);
+}
+
+function swap(index1: number, index2: number) {
+  const temp = this[index1];
+  this[index1] = this[index2];
+  this[index2] = temp;
+  triggerRerender(this);
+}
+
+
+
+
+
 export function UseList(initial_data: any[]) {
   const [_, setPlaceHolder] = useState(0);
 
-  
   useEffect(() => {
-    const key = current_key++;
-    if (!rerenders.get(initial_data)) {
-      rerenders.set(initial_data, new Map());
-    }
-    rerenders
+      const key = current_key++;
+      if (!rerenders.get(initial_data)) {
+        rerenders.set(initial_data, new Map());
+      }
+      rerenders
       .get(initial_data)
       .set(key, () => setPlaceHolder((prev) => prev + 1));
-
-    return () => {
-      rerenders.get(initial_data).delete(key);
-    };
+      
+      return () => {
+        rerenders.get(initial_data).delete(key);
+      };
   });
-
-  function triggerRerender() {
-    for (const rerender of rerenders.get(initial_data).values()) {
-      console.log({ rerender });
-      rerender();
-    }
-  }
-
-  function deleteUser(id: number | any) {
-    for (let i = 0; i < initial_data.length; i++) {
-      if (initial_data[i].id === id) {
-        initial_data.splice(i, 1);
-        break;
-      }
-    }
-    triggerRerender();
-  }
-  function addNewItem() {
-    const newUser = {
-      id: initial_data.length + 1,
-      name: "Abhi",
-      age: 21,
-      country: "India",
-      city: "Delhi",
-    };
-    initial_data.concat(newUser);
-    triggerRerender();
-  }
-  function sortList(field: string) {
-    initial_data.sort((a, b) => (a[field] > b[field] ? 1 : -1));
-    triggerRerender();
-  }
-  function clearList() {
-    initial_data.splice(0, initial_data.length);
-    triggerRerender();
-  }
-  function updateItem(id: number | any, key: string, value: any | Function) {
-    for (let i = 0; i < initial_data.length; i++) {
-      if (initial_data[i].id === id) {
-        if (typeof value === "function") {
-          initial_data[i][key] = value(initial_data[i][key]);
-        } else {
-          initial_data[i][key] = value;
-        }
-        break;
-      }
-    }
-    triggerRerender();
-  }
-
-  function swap(index1: number, index2: number) {
-    const temp = initial_data[index1];
-    initial_data[index1] = initial_data[index2];
-    initial_data[index2] = temp;
-    triggerRerender();
-  }
 
   return [
     initial_data,
-    deleteUser,
-    addNewItem,
-    sortList,
-    clearList,
-    updateItem,
-    swap,
+    deleteUser.bind(initial_data),
+    addNewItem.bind(initial_data),
+    sortList.bind(initial_data),
+    clearList.bind(initial_data),
+    updateItem.bind(initial_data),
+    swap.bind(initial_data),
   ];
 }
